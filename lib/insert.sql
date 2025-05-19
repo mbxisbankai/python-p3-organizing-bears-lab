88de11ed869b27fb394d1d11ddec768b48c66018
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-05-19 12:56:52 +0300

    Organizing Bears Lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+  ('Mr. Chocolate', 2, 'M', 'Brown', 'Calm', TRUE),
+  ('Rowdy', 4, 'M', 'Black', 'Aggressive', TRUE),
+  ('Tabitha', 3, 'F', 'Golden', 'Gentle', TRUE),
+  ('Sergeant Brown', 5, 'M', 'Dark Brown', 'Strict', TRUE),
+  ('Melissa', 6, 'F', 'White', 'Curious', TRUE),
+  ('Grinch', 7, 'M', 'Gray', 'Grumpy', FALSE),
+  ('Wendy', 3, 'F', 'Black', 'Playful', TRUE),
+  (NULL, 1, 'M', 'Brown', NULL, FALSE);
